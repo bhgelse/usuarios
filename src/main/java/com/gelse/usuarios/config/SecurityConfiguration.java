3716e5de0afd06983511c38620158c27167fb650
bhgelse <bhgelse@gmail.com>
2025-08-06 21:15:36 -0500

    configurando el orden de los filtros y autorizando a el usuario

--- src/main/java/com/gelse/usuarios/config/SecurityConfiguration.java
+++ src/main/java/com/gelse/usuarios/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.gelse.usuarios.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +12,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private SecurityFilter securityFilter;
     // 2
     // se deshabilita el usu de cookies y se establece la politica de inicio de secion
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> {
+                        request.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                        request.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     // 4
--- src/main/java/com/gelse/usuarios/config/SecurityFilter.java
+++ src/main/java/com/gelse/usuarios/config/SecurityFilter.java
@@ -1,14 +1,20 @@
 package com.gelse.usuarios.config;
 
+import com.gelse.usuarios.model.Usuario;
+import com.gelse.usuarios.repository.IUsuarioRepositoy;
 import com.gelse.usuarios.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.security.auth.Subject;
 import java.io.IOException;
 
 @Component
@@ -16,21 +22,30 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private IUsuarioRepositoy usuarioRepositoy;
     // 8
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String tokenJWT = recuperarToken(request);
-        String subject = tokenService.getSubject(tokenJWT);
-        System.out.println(subject);
+        if (tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails usuario = usuarioRepositoy.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         //cadena de filtros
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("Token JWT no enviado en el encabezado de Authorizacion");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ","" );
+        return null;
     }
 }
